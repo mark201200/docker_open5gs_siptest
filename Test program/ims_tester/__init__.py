af4a379de92456dbe5a88d4f63126c340c4d817d
Marco Altomare <alto.marco@gmail.com>
2026-05-11 23:39:57 +0200

    modularizzato

--- Test program/ims_tester/__init__.py
+++
@@ -1,5 +0,0 @@
-"""IMS thesis tester package."""
-
-from .cli import main
-
-__all__ = ["main"]
--- Test program/main.py
+++
@@ -1,4 +0,0 @@
-from ims_tester.cli import main
-
-if __name__ == "__main__":
-    raise SystemExit(main())
